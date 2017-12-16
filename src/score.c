1087384c42974c33bd1109217491749c23a33e37
Alexis BIZON <alexis.bizon@imt-atlantique.net>
2017-12-16 18:44:06 +0100

    Version initiale print_scores() partie TERMINAL

--- src/score.c
+++ src/score.c
@@ -4,9 +4,9 @@ void get_surname_player(char* surname, scoreScanType in)
 {
 	switch(in){
 		case TERMINAL:
-			printf("Veuillez saisir votre nom : /n");
+			printf("Veuillez saisir votre nom : \n");
 			scanf("%s", surname);
-			printf("Votre nom est : %s/n", surname);
+			printf("Votre nom est : %s\n", surname);
 			break;
 		case GUI:
 			sprintf(surname, "ERROR");
@@ -19,7 +19,28 @@ void get_surname_player(char* surname, scoreScanType in)
 
 void print_scores(score* scores, uint32_t nbScores, scorePrintType out)
 {
-
+	switch(in){
+		case TERMINAL:
+			if(nbScores<=0)
+			{
+				printf("Aucun score à afficher\n");
+				break;
+			}
+			printf("Liste des Scores : \n");
+			uint32_t iScore = 0;
+			do{
+				printf("\t%s : %d\n", scores[iScore]->name, scores[iScore]->score);
+				iScore++;
+			}while(iScore<nbScores);
+			printf("Fin des scores")
+			break;
+		case GUI:
+			printf("ERROR");
+			break;
+		default:
+			printf("ERROR");
+			break;
+	}
 }
 
 void write_score_file(char* path, score* scores, uint32_t nbScores)
